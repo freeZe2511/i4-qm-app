69c783a9bd5e19f2d88950247b2cc48408ecf98a
Tim Eggers <tim.luca.eggers@mni.thm.de>
2022-07-06 18:24:22 +0200

    recipes docs

--- I4-QM-app/I4-QM-app/Helpers/NumericValidationBehavior.cs
+++ I4-QM-app/I4-QM-app/Helpers/NumericValidationBehavior.cs
@@ -6,6 +6,7 @@ namespace I4_QM_app.Helpers
     /// <summary>
     /// Behavior for numeric validation for entries.
     /// </summary>
+    // https://stackoverflow.com/questions/44475667/is-it-possible-specify-xamarin-forms-entry-numeric-keyboard-without-comma-or-dec
     public class NumericValidationBehavior : Behavior<Entry>
     {
         /// <inheritdoc/>

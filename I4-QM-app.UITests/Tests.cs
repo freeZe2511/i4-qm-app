66e6ae80e40b42d95db37be58deaae0533b5b5ff
Tim Eggers <tim.luca.eggers@mni.thm.de>
2022-07-08 12:54:05 +0200

    tests init

---
+++ I4-QM-app.UITests/Tests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using Xamarin.UITest;
+
+namespace I4_QM_app.UITests
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void LoginDisabled()
+        {
+            app.WaitForElement(c => c.Marked("LoginEntry"));
+            app.EnterText(c => c.Marked("LoginEntry"), "12");
+            app.Tap("LoginButton");
+            var res = app.Query(x => x.Marked("LoginButton")).FirstOrDefault().Enabled;
+            Assert.IsFalse(res);
+
+        }
+
+        [Test]
+        public void SuccessfulLogin()
+        {
+            app.WaitForElement(c => c.Marked("LoginEntry"));
+            app.EnterText(c => c.Marked("LoginEntry"), "1234");
+
+            app.Tap("LoginButton");
+
+        }
+    }
+}

25629c05c57545782331b7ec1d690e9ff67877bf
Tim Eggers <tim.luca.eggers@mni.thm.de>
2022-07-11 10:13:59 +0200

    flyout/menu item refactor+doc

---
+++ I4-QM-app.NUnitTests/Tests/Helpers/NumericValidationBehaviorTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Xamarin.Forms;
+
+namespace I4_QM_app.NUnitTests.Tests.Helpers
+{
+    internal class NumericValidationBehaviorTest
+    {
+        [Test]
+        public void OnAttach()
+        {
+            var e = new Entry();
+            var a = new NumericValidationBehaviorTestable();
+            a.OnAttachedToTestable(e);
+            Assert.IsNotNull(a);
+        }
+    }
+}
---
+++ I4-QM-app.NUnitTests/Tests/Helpers/NumericValidationBehaviorTestable.cs
@@ -0,0 +1,18 @@
+﻿using I4_QM_app.Helpers;
+using Xamarin.Forms;
+
+namespace I4_QM_app.NUnitTests.Tests.Helpers
+{
+    internal class NumericValidationBehaviorTestable : NumericValidationBehavior
+    {
+        public void OnAttachedToTestable(Entry entry)
+        {
+            base.OnAttachedTo(entry);
+        }
+
+        public void OnDetachingFromTestable(Entry entry)
+        {
+            base.OnDetachingFrom(entry);
+        }
+    }
+}
